36faaa453cb86c337ea59ae67cc7aa69edcf98c5
Gustavo Borges <ggb@cin.ufpe.br>
2024-01-10 18:43:42 -0300

    feat: [desafio concluído] Teste das classes Smartphone e suas classes filhas (Nokia e Iphone)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "NK1", imei:"NK11111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "NK1", imei:"NK11111", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
